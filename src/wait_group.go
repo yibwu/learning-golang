62dd699e2f24fb8949f5ed02b46a2802cf3abcb3
Yibo Wu <wuyibo7788@163.com>
2019-07-25 23:16:23 +0800

    wait group demo

---
+++ src/wait_group.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go foo(i)
+	}	
+	wg.Wait()
+}
+
+func foo(i int) {
+	fmt.Println(i)
+	wg.Done()
+}
